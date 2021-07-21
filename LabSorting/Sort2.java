899098eed989000df30c3688ae03df22a017a39d
MarkKh <85799327+MarkKh@users.noreply.github.com>
2021-07-21 11:26:20 +0700

    Add files via upload

---
+++ LabSorting/Sort2.java
@@ -0,0 +1,26 @@
+class Sort2 {
+    public static void main(String[] args) {
+        int data[] = { 4, 2, 15, 9, 12, 5, 10, 1 };
+        int out[] = new int[data.length];
+        for (int i = 0; i < data.length; i++)
+            out[i] = 0;
+        int id = 0;
+        for (int i = 0; i < data.length; i++) {
+            id = findMin(data);
+            out[i] = data[id];
+            data[id] = 99999999;
+        }
+    }// main
+
+    public static int findMin(int[] d) {
+        int min = d[0];
+        int id = 0;
+        for (int i = 1; i < d.length; i++) {
+            if (d[i] < min) {
+                min = d[i];
+                id = i;
+            }
+        }
+        return id;
+    }
+}// class
